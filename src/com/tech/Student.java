3874c8ef6c180a0094c22169596b444df05c8a67
swapnil-sm-92 <swapnilmatole@gmail.com>
2025-10-15 12:33:01 +0530

    Second coomit

--- src/com/tech/Student.java
+++ src/com/tech/Student.java
@@ -5,6 +5,7 @@ public class Student {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Student data 1");
+		System.out.println("Student data 2");
 	}
 
 }
